d26818c4635b944f4d1937de8a79e8016834a3eb
Kenneth Davis <kennethdavis391@gmail.com>
2020-01-02 08:11:53 -0500

    minor model changes, mutamon controller made

---
+++ MutaMonApi/src/main/java/com/revature/services/MutamonService.java
@@ -0,0 +1,13 @@
+package com.revature.services;
+
+import java.util.List;
+
+import com.revature.models.Monster;
+
+public interface MutamonService {
+
+	public List<Monster> findMonstersByUserIdAndLevel(int id, int level);
+	
+	public List<Monster> findAllMonsters();
+	
+}
