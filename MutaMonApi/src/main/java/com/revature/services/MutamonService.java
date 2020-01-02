756957b447264e6d6400bab43fa63c65e59ec0e7
Dane Vosmeier <dane.vosmeier@revature.net>
2020-01-02 11:52:22 -0500

    added get monster by userid

--- MutaMonApi/src/main/java/com/revature/services/MutamonService.java
+++ MutaMonApi/src/main/java/com/revature/services/MutamonService.java
@@ -9,5 +9,7 @@ public interface MutamonService {
 	public List<Monster> findMonstersByUserIdAndLevel(int id, int level);
 	
 	public List<Monster> findAllMonsters();
+
+	public Monster findMonsterByUserId(int id);
 	
 }
